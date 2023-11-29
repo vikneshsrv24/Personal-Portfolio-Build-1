7a16890de85d00d213c688ee2447af973331ada9
SRV <vikneshsrv24@gmail.com>
2023-11-29 04:29:19 -0800

    js function for hanburger added

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){                                                       // getting function for the buttons om the page
+    const menu=document.querySelector(".menu-links");                            // query selector used to select query in a webpage and make action on that
+    const icon=document.querySelector(".logomenu");                      
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");                                        // return CSS classes in the form of an array
+}
